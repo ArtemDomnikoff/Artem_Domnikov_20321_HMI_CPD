4a9c84c89f92d346c4165c8a8a94c9b3618d3ad0
ArtemDomnikoff <adomnikov3@gmail.com>
2024-05-14 10:51:08 +0300

    Song 12 days of Christmas

---
+++ CPD/Second semester/task_7/song/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let days = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth",
+        "eleventh", "twelfth",
+    ];
+    let gifts = [
+        "A partridge in a pear tree",
+        "Two turtle doves, and",
+        "Three French hens,",
+        "Four calling birds,",
+        "Five golden rings,",
+        "Six geese a-laying,",
+        "Seven swans a-swimming,",
+        "Eight maids a-milking,",
+        "Nine ladies dancing,",
+        "Ten lords a-leaping,",
+        "Eleven pipers piping,",
+        "Twelve drummers drumming,",
+    ];
+    for day in 0..12 {
+        println!("On the {} day of Christmas,", days[day]);
+        println!("My true love gave to me:");
+        
+        for gift_day in (0..=day).rev() {
+            if gift_day == 0 && day != 0 {
+                print!("And ");
+            }
+            println!("{}", gifts[gift_day]);
+        }
+        println!();
+    }
+}
